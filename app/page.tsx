96e040707dca9d386524fe930dead4c3899dec06
Kleascm <kleascm@gmail.com>
2025-03-20 10:54:13 +1000

    feat: enhance styling for main components and improve layout responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 "use client";
+
 import InputBox from '@/components/InputBox';
 import OutputBox from '@/components/OutputBox';
 import { useState } from 'react';
+import styles from '@/styles/page.module.scss';
 
 export default function Home() {
   const [response, setResponse] = useState('');
 
   return (
-    <main>
+    <main className={styles.container}>
       <h1>Shandris</h1>
-      <InputBox />
       <OutputBox output={response} />
+      <InputBox />
     </main>
   );
 }
